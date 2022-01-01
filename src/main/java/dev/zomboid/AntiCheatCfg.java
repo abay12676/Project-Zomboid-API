ee310f8e006954584ac6db684a8dfe9b9cd80a72
ionutlala <ionutlala@github.com>
2022-01-01 16:44:20 -0500

    future proofing

--- src/main/java/dev/zomboid/AntiCheatCfg.java
+++ src/main/java/dev/zomboid/AntiCheatCfg.java
@@ -1,5 +1,6 @@
 package dev.zomboid;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 import zombie.network.PacketTypes;
 
@@ -12,52 +13,62 @@ public class AntiCheatCfg {
     /**
      * The link to the discord API to report to.
      */
+    @SerializedName("discordApi")
     public String discordApi = "";
 
     /**
      * If true, the player ids in packets will be overwritten with the player id from the UDP connection
      * if a valid player id is not found in the UDP connection's player array.
      */
+    @SerializedName("enforcePacketOwnership")
     private boolean enforcePacketOwnership = true;
 
     /**
      * If true, the sync perks packet will be validated to ensure no cheating is detected.
      */
+    @SerializedName("enforceSyncPerks")
     private boolean enforceSyncPerks = false;
 
     /**
      * If true, teleports will cause violations unless the player is a mod/admin.
      */
+    @SerializedName("enforceTeleport")
     private boolean enforceTeleport = true;
 
     /**
      * If true, extra info packets will cause violations unless the player is a mod/admin.
      */
+    @SerializedName("enforceExtraInfo")
     private boolean enforceExtraInfo = true;
 
     /**
      * If true, player death packets must be a player associated with the network connection or a violation will be caused.
      */
+    @SerializedName("enforcePlayerDeaths")
     private boolean enforcePlayerDeaths = true;
 
     /**
      * The difference in value that a perk change must hit in order to cause a violation.
      */
+    @SerializedName("enforceSyncThreshold")
     private int enforceSyncThreshold = 10;
 
     /**
      * If true, additional pain packets will cause a violation.
      */
+    @SerializedName("enforceAdditionalPain")
     private boolean enforceAdditionalPain = true;
 
     /**
      * If rate limiting is enabled.
      */
+    @SerializedName("rateLimiting")
     private boolean rateLimiting = true;
 
     /**
      * A list of rate limits to apply.
      */
+    @SerializedName("rateLimits")
     private List<AntiCheatRateLimit> rateLimits = new LinkedList<>();
 
     public boolean isRateLimited(short type) {
--- src/main/java/dev/zomboid/AntiCheatRateLimit.java
+++ src/main/java/dev/zomboid/AntiCheatRateLimit.java
@@ -1,11 +1,15 @@
 package dev.zomboid;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 
 @Data
 public class AntiCheatRateLimit {
 
+    @SerializedName("type")
     private String type;
+
+    @SerializedName("delay")
     private long delay;
 
 }
