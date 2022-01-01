707142f6537abab4fad7874f731b57a0d4627313
ionutlala <ionutlala@github.com>
2022-01-01 16:45:44 -0500

    comments

--- src/main/java/dev/zomboid/AntiCheatRateLimit.java
+++ src/main/java/dev/zomboid/AntiCheatRateLimit.java
@@ -3,12 +3,22 @@ package dev.zomboid;
 import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 
+/**
+ * A rate limit to be applied by the anti-cheat.
+ */
 @Data
 public class AntiCheatRateLimit {
 
+    /**
+     * The name of the enum field that represents the packet
+     * to rate limit. These can be found in {@link zombie.network.PacketTypes.PacketType}.
+     */
     @SerializedName("type")
     private String type;
 
+    /**
+     * The number of milliseconds a player must wait in between packets.
+     */
     @SerializedName("delay")
     private long delay;
 
